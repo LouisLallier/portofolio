773494d9b279cfbbb84e2c4f21bb4b6315908f63
LouisLallier <86591552+LouisLallier@users.noreply.github.com>
2023-04-07 14:39:47 +0200

    experience section 70%

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -8,7 +8,27 @@ type Props = {
 const Skill = ({ directionLeft }: Props) => {
   return (
     <div className={"group relative flex cursor-pointer"}>
-      <motion.img src="https://res.cloudinary.com/dpeyhfmjh/image/upload/v1680804371/production/yarn_logo_edkkp6.png" />
+      <motion.img
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src="https://res.cloudinary.com/dpeyhfmjh/image/upload/v1680804371/production/yarn_logo_edkkp6.png"
+        className={
+          "rounded-full border border-gray-500 object-cover w-24 h-24 filter group-hover:grayscale transition duration-300 ease-in-out"
+        }
+      />
+      <div
+        className={
+          "absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out group-hover:bg-white rounded-full h-24 w-24 z-0"
+        }
+      >
+        <div className={"flex items-center justify-center h-full"}>
+          <p className={"text-2xl font-bold text-black opacity-100"}>100%</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -29,7 +30,10 @@ const Home: NextPage = () => {
       <section id="skills" className={"snap-center"}>
         <Skills />
       </section>
-      {/*    PROJECT*/}
+      {/*    PROJECTS*/}
+      <section id="projects" className={"snap-center"}>
+        <Projects />
+      </section>
       {/*    CONTACT ME*/}
     </div>
   );
